bcacf6d1c0e088225c57af30b713e556c1625071
Tim Huijsmans <timhuijsmans@mbp-van-tim.home>
2021-01-21 18:49:24 +0100

    set up star size

--- circular_mask.py
+++ circular_mask.py
@@ -19,7 +19,7 @@ def r_theta(im, xc, yc):
     return(rr, phi)
 
 
-def star_array(radius, matrix_size):
+def star_array_limb(radius, matrix_size):
 
     star = np.zeros((matrix_size, matrix_size))
     cx = matrix_size / 2
@@ -37,7 +37,7 @@ def star_array(radius, matrix_size):
     a4 = coefficient_array[3]
 
     for x in range(matrix_size):
-        print("row number {}" .format(x))
+        #print("row number {}" .format(x))
         for y in range(matrix_size): 
             
             r = math.sqrt((x - cx)**2 + (y - cy)**2)
@@ -47,6 +47,23 @@ def star_array(radius, matrix_size):
                 I = 1 - a1*(1-mu**0.5) - a2*(1-mu) - a3*(1-mu**1.5) - a4*(1-mu**2)
                 star[y][x] = I # set the value of this point according to the value of the intensity determined by the non linear formula
 
+    return star
+
+def star_array_no_limb(radius, matrix_size):
+
+    star = np.zeros((matrix_size, matrix_size))
+    cx = matrix_size / 2
+    cy = matrix_size / 2
+    cr = radius
+
+    for x in range(matrix_size):
+        #print("row number {}" .format(x))
+        for y in range(matrix_size): 
+            
+            r = math.sqrt((x - cx)**2 + (y - cy)**2)
+            
+            if r < cr:
+                star[y][x] = 1 # set the value of this point according to the value of the intensity determined by the non linear formula
     return star
 
 def planet_array(radius):
@@ -60,19 +77,22 @@ def planet_array(radius):
     return r_planet
 
 def star_mask_by_planet(star_array, cx, cy, r_planet, star_brightness):
-
     # flip the y-coordinate to correct for the upside down orientation
-    cy_projection = len(star_array) - 1 - cy
+    cy_flip = len(star_array) - 1 - cy
     matrix_size = len(r_planet)
 
     # set the part of the matrix to be replaced
-    ymin = cy_projection - matrix_size / 2
-    ymax = cy_projection + matrix_size / 2
+    ymin = cy_flip - matrix_size / 2
+    ymax = cy_flip + matrix_size / 2
     xmin = cx - matrix_size / 2
     xmax = cx + matrix_size / 2
 
     # check brightness difference for small part of the matrix
     old_matrix = np.array(star_array[ymin:ymax + 1, xmin:xmax + 1])
+    
+    # star_array[ymin:ymax + 1, xmin:xmax + 1] = old_matrix * r_planet
+    # brightness = np.sum(star_array)
+    
     brightness = star_brightness - (np.sum(old_matrix) - np.sum(old_matrix * r_planet))
     star_array[ymin:ymax + 1, xmin:xmax + 1] = old_matrix
 
@@ -80,7 +100,7 @@ def star_mask_by_planet(star_array, cx, cy, r_planet, star_brightness):
 
 if __name__ == "__main__":
     # parameters of star and planet (probably don't want to go much beyond 300 planet and 4000 star)
-    planet_radius = 10
+    planet_radius = 2
     star_radius = 3000
     
     # translated input into matrix sizes
@@ -90,8 +110,8 @@ if __name__ == "__main__":
     # initialize output
     brightness_profile = []
 
-    # create the star
-    star = star_array(star_radius, star_array_size)
+    # create the star (remove no in case you wish to use limb darkening)
+    star = star_array_limb(star_radius, star_array_size)
     star_brightness = np.sum(star)
 
     # create matrix of the planet
@@ -99,11 +119,8 @@ if __name__ == "__main__":
 
     # because we import the kepler function's variable coordinate, we can call it here the format is a list of coordinate lists: [[x1,y1], [x2,y2], ....]
     kepler_coordinates = np.array(kepler_output)
-    scaled_kepler = kepler_coordinates * (star_radius / (2 * (10 ** -7)))
+    scaled_kepler = kepler_coordinates * (star_radius / (0.03))
     translated_kepler = [x+(star_array_size / 2) for x in scaled_kepler]
-    print(scaled_kepler)
-    print(translated_kepler)
-    print(star_array_size)
 
     # This for loop is now used to move the planet through the matrix. If we have useful Kepler output, we can instead loop over the xy-coordinates in the output
     count = 0
@@ -111,7 +128,7 @@ if __name__ == "__main__":
         brightness = star_mask_by_planet(star, int(coord[0]), int(coord[1]), r_planet, star_brightness)
         brightness_profile.append(brightness)
         count+=1
-        #print("planet step: {}" .format(count))
+        print("planet step: {}" .format(count))
 
     plt.plot(brightness_profile)
     plt.title("Lightcurve of a transit with limb-darkening for r-star: %s pixels and r-planet: %s pixels (w = 180 degrees)" % (star_radius, planet_radius))
--- kepler_test.py
+++ kepler_test.py
@@ -34,7 +34,7 @@ def eccentric_anomaly(e, P, M):
     i = 0
     dif = 1
 
-    while dif > 0.01:
+    while dif > 0.001:
         M_index = E_value[i] - e * math.sin(E_value[i])
         E_index = E_value[i] + ((M - M_index) / (1 - e * math.cos(E_value[i])))
         E_value.append(E_index)
@@ -70,8 +70,8 @@ def kepler_cropped(a, i, w, omega, P, T, e, timestep):# loop over timesteps unti
         if t == 0:    
             old_x = rect_coord(TI, e, E)[0]
 
-        # cut out only the small transit part
-        if rect_coord(TI, e, E)[0] > old_x and t != 0 and rect_coord(TI, e, E)[0] > (1.2 * (-2) * (10 ** -7)) and rect_coord(TI, e, E)[0] < (1.2 * (2) * (10 ** -7)):
+        # cut out only the small transit part: the last two statements determine the size of the star (and the margin aroung the star)
+        if rect_coord(TI, e, E)[0] > old_x and t != 0 and rect_coord(TI, e, E)[0] > (1.8 * (-0.02)) and rect_coord(TI, e, E)[0] < (1.8 * (0.02)):
             
             x_coordinates.append(rect_coord(TI, e, E)[0])
             y_coordinates.append(rect_coord(TI, e, E)[1])
@@ -106,36 +106,36 @@ def kepler(a, i, w, omega, P, T, e, timestep):# loop over timesteps until one tr
             
         coordinates.append([rect_coord(TI, e, E)[0], rect_coord(TI, e, E)[1]])
         
-    plt.plot(t_list, E_list)
-    plt.show()
+    #plt.plot(t_list, E_list)
+    #plt.show()
     return x_coordinates, y_coordinates, coordinates
 
 # initial conditions
 a = 0.3 #boogseconden
-i = math.radians(0)
+i = math.radians(270)
 w = math.radians(0) 
 omega = math.radians(0)
 P = 1 
-T = 0.15 #time passage through periastron (seconden?) #where the planet starts
-e = 0.28
-timestep = 1./24
+T = 0.5 #time passage through periastron (seconden?) #where the planet starts
+e = 0
+timestep = 0.0001
 
 # you can choose here if you would like to run the cropped version or the regular orbit
-#coord = kepler_cropped(a, i, w, omega, P, T, e, timestep)
-coord = kepler(a, i, w, omega, P, T, e, timestep)
+coord = kepler_cropped(a, i, w, omega, P, T, e, timestep)
+#coord = kepler(a, i, w, omega, P, T, e, timestep)
 x_coordinates = coord[0]
 y_coordinates = coord[1]
 coordinates = coord[2]
 
-# if __name__ == "__main__":
-#     for i in range(0,len(coordinates)):
-#         plt.plot(0,0,'yo', markersize=10)
-#         plt.plot(x_coordinates[0], y_coordinates[0], 'ro', markersize=5,label='start')
-#         plt.plot(x_coordinates[:i],y_coordinates[:i],'bo', markersize=1)
-#         plt.plot(x_coordinates[i], y_coordinates[i], "go", markersize = 10)
-#         plt.draw()
-#         plt.pause(0.001)
-#         plt.clf()
-#         plt.xlim(-0.6,0.4)
-#         plt.ylim(-0.4,0.4)
-#     plt.show()
+if __name__ == "__main__":
+    for i in range(0,len(coordinates)):
+        plt.plot(0,0,'yo', markersize=10)
+        plt.plot(x_coordinates[0], y_coordinates[0], 'ro', markersize=5,label='start')
+        plt.plot(x_coordinates[:i],y_coordinates[:i],'bo', markersize=1)
+        plt.plot(x_coordinates[i], y_coordinates[i], "go", markersize = 10)
+        plt.draw()
+        plt.pause(0.001)
+        plt.clf()
+        plt.xlim(-0.6,0.4)
+        plt.ylim(-0.4,0.4)
+    plt.show()
