d81168aaafa498f9bc3a43587f950c441ed72b3f
timhuijsmans1 <timhuijsmans@hotmail.com>
2022-10-11 19:58:23 +0100

    cleaned up kepler file

--- kepler_test.py
+++ kepler_test.py
@@ -3,37 +3,39 @@ from matplotlib import pyplot as plt
 import math
 import copy
 
- 
 
-#Step 1: Calculate the Thiele Innes parameters, independent of eccentricity
 def Thiele_Innes(a, i, w, omega):
+    """
+    Calculates the Thiele Innes parameters, independent of eccentricity
+    """
 
-    A = a*(math.cos(w) * math.cos(omega) - math.sin(w) * math.sin(omega) * math.cos(i))
-    B = a*(math.cos(w) * math.sin(omega) + math.sin(w) * math.cos(omega) * math.cos(i))
+    A = a * (math.cos(w) * math.cos(omega) - math.sin(w) * math.sin(omega) * math.cos(i))
+    B = a * (math.cos(w) * math.sin(omega) + math.sin(w) * math.cos(omega) * math.cos(i))
 
-    F = a*(-math.sin(w) * math.cos(omega) - math.cos(w) * math.sin(omega) * math.cos(i))
-    G = a*(-math.sin(w) * math.sin(omega) + math.cos(w) * math.cos(omega) * math.cos(i))
+    F = a * (-math.sin(w) * math.cos(omega) - math.cos(w) * math.sin(omega) * math.cos(i))
+    G = a * (-math.sin(w) * math.sin(omega) + math.cos(w) * math.cos(omega) * math.cos(i))
 
     return A, B, F, G
 
- 
-#Step 2: Calculate the mean anomaly M, before you can calculate eccentric anomaly E
 def mean_anomaly(P, t, T):
-
-    M = (2*math.pi/P) * (t - T)
+    """
+    Calculate the mean anomaly M, before you can calculate eccentric anomaly E
+    """
+    M = (2 * math.pi / P) * (t - T)
 
     return M
 
- 
-#Step 3: calculate the eccentric anomaly E with the mean anomaly 
 def eccentric_anomaly(e, P, M):    
-
-    E_0 = M + e * math.sin(M) + e ** 2 / M * math.sin(2*M)
+    """
+    calculate the eccentric anomaly E with the mean anomaly
+    """
+    E_0 = M + e * math.sin(M) + e ** 2 / M * math.sin(2 * M)
     E_value = [E_0]
 
     i = 0
     dif = 1
 
+    # numerical computation of eccentric anomaly
     while dif > 0.001:
         M_index = E_value[i] - e * math.sin(E_value[i])
         E_index = E_value[i] + ((M - M_index) / (1 - e * math.cos(E_value[i])))
@@ -43,9 +45,12 @@ def eccentric_anomaly(e, P, M):
 
     return E_value[-1]
 
-#Step 4: calculate the rectengular coordinates with results form step 1, 2 and 3
-def rect_coord(TI, e, E):
 
+def rect_coord(TI, e, E):
+    """
+    calculates the rectangular coordinates of planet 
+    with results form step 1, 2 and 3
+    """
     X = math.cos(E) - e
     Y = math.sqrt(1 - e ** 2) * math.sin(E)
 
@@ -53,25 +58,34 @@ def rect_coord(TI, e, E):
     y_pos = TI[1]*X + TI[3]*Y
     return x_pos,y_pos
 
-def kepler_cropped(a, i, w, omega, P, T, e, timestep, crop_range):# loop over timesteps until one transit has passed
+def kepler_cropped(a, i, w, omega, P, T, e, timestep, crop_range):
 
     TI = Thiele_Innes(a, i, w, omega)
 
     coordinates = []
     x_coordinates = []
     y_coordinates = []
-        
+
+    # loop over timesteps until exactly 1 transit has passed
     for t in np.arange (0, 1, timestep):
         M = mean_anomaly(P, t, T)
         E = eccentric_anomaly(e, P, M)
         
-        # This is a little hacky because the first coordinate will never be added alhough it might be moving positive x,
+        # This is a hack because the first coordinate will never be added 
+        # although it might be moving in positive x direction,
         # but it works for now as long as we choose T accordingly
         if t == 0:    
             old_x = rect_coord(TI, e, E)[0]
-            print(old_x)
-        # cut out only the small transit part: the last two statements determine the size of the star (and the margin aroung the star)
-        if rect_coord(TI, e, E)[0] > old_x and t != 0 and rect_coord(TI, e, E)[0] > (-crop_range) and rect_coord(TI, e, E)[0] < (crop_range):
+        
+        cond_1 = t != 0 # time condition
+
+        # cut out the part of the matrix concerning the transit 
+        # 3+4 are conditions for the star and margin sizes
+        cond_2 = rect_coord(TI, e, E)[0] > old_x 
+        cond_3 = rect_coord(TI, e, E)[0] > (-crop_range) 
+        cond_4 = rect_coord(TI, e, E)[0] < (crop_range)
+        
+        if cond_1 and cond_2 and cond_3 and cond_4:
 
             x_coordinates.append(rect_coord(TI, e, E)[0])
             y_coordinates.append(rect_coord(TI, e, E)[1])
@@ -100,7 +114,6 @@ def kepler(a, i, w, omega, P, T, e, timestep):# loop over timesteps until one tr
         M_list.append(M)
         t_list.append(t)
 
-
         x_coordinates.append(rect_coord(TI, e, E)[0])
         y_coordinates.append(rect_coord(TI, e, E)[1])
             
@@ -110,19 +123,6 @@ def kepler(a, i, w, omega, P, T, e, timestep):# loop over timesteps until one tr
     #plt.show()
     return x_coordinates, y_coordinates, coordinates
 
-# initial conditions
-a = 0.3 #AU
-i = math.radians(90)
-w_angle = math.radians(180) 
-omega = math.radians(0)
-P = 0.5 # period of half a year
-T = -0.01 #time passage through periastron (seconden?) #where the planet starts
-e = 0
-timestep = (0.000003802651) # 1 minutes when P is set to 0.5 yrs
-
-r_star = 0.01 * a
-crop_range = r_star * 1.3
-
 if __name__ == "__main__":
     # initial conditions
     a = 0.3 #AU
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     w_angle = math.radians(180) 
     omega = math.radians(0)
     P = 0.5 # period of half a year
-    T = -0.01 #time passage through periastron (seconden?) #where the planet starts
+    T = -0.01 #time passage through periastron where the planet starts in seconds
     e = 0.1
     timestep = (0.0001) # 1 minutes when P is set to 0.5 yrs
 
